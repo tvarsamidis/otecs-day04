b3c722112c08d32f3c54af8e92b5cae3dd48beb8
Thomas Varsamidis <t.varsamidis@codehub.gr>
2024-10-08 12:52:12 +0300

    Added getter and setter for age

--- src/MainApplication.java
+++ src/MainApplication.java
@@ -1,18 +1,34 @@
 public class MainApplication {
 
     public static void showData(Person person) {
-        System.out.println("Name=" + person.name);
+        System.out.println("Name=" + person.firstName);
         System.out.println("Height=" + person.height);
+        System.out.println("Age=" + person.getMyAge());
     }
 
     public static void main(String[] args) {
         Person winner = new Person();
-        winner.name = "Helen";
+        winner.firstName = "Helen";
+        winner.lastName = "Anastasopoulos";
         winner.height = 167;
         winner.weight = 65;
         winner.isMarried = true;
+        winner.changeAgeTo(43);
+        winner.oneYearOlder();
+        winner.oneYearOlder();
+        winner.oneYearOlder();
+        winner.oneYearOlder();
+        winner.oneYearOlder();
+        winner.oneYearOlder();
+        winner.oneYearOlder();
+        winner.oneYearOlder();
+        winner.oneYearOlder();
+        winner.oneYearOlder();
+        winner.oneYearOlder();
+        winner.oneYearOlder();
+
         showData(winner);
-        System.out.println(winner.name + " has a bmi of " + winner.calculateBmi());
+        System.out.println(winner.fullName() + " has a bmi of " + winner.calculateBmi());
     }
 
 
--- src/Person.java
+++ src/Person.java
@@ -3,20 +3,37 @@ public class Person {
     public static final int RETIREMENT_AGE = 67;
     public static final int MAX_NUMBER_OF_SPOUSES = 1;
 
-    public String name;
+    public String firstName;
+    public String lastName;
     public int height;
     public int weight;
     public boolean isMarried;
-    public int age;
+    private int age;
 
-    public static double staticCalculateBmi(Person person) {
-        double heightInMeters = person.height / 100.0;
-        double bmi = person.weight / heightInMeters / heightInMeters;
-        return bmi;
+    public String fullName() {
+        return firstName + " " + lastName;
     }
-    public  double calculateBmi() {
+
+    public double calculateBmi() {
         double heightInMeters = height / 100.0;
         double bmi = weight / heightInMeters / heightInMeters;
         return bmi;
     }
+
+    public void oneYearOlder() {
+        age++;
+    }
+
+    public void changeAgeTo(int newAge){
+        if (newAge >= 1 && newAge <= 120) {
+            age = newAge;
+        }
+    }
+
+    public int getMyAge() {
+        return age < 60 ? age : 60;
+    }
+
+
+
 }
