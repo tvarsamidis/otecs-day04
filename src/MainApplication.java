7535fa89cb532f9d70f3b1d137ebcf0a3c4b6a8e
Thomas Varsamidis <t.varsamidis@codehub.gr>
2024-10-08 12:22:25 +0300

    Added static BMI

--- src/MainApplication.java
+++ src/MainApplication.java
@@ -5,13 +5,21 @@ public class MainApplication {
         System.out.println("Height=" + person.height);
     }
 
-
     public static void main(String[] args) {
         Person winner = new Person();
         winner.name = "Helen";
         winner.height = 167;
+        winner.weight = 65;
         winner.isMarried = true;
         showData(winner);
+        double bmi = calculateBmi(winner);
+        System.out.println(winner.name + " has a bmi of " + bmi);
+    }
+
+    private static double calculateBmi(Person person) {
+        double heightInMeters = person.height / 100.0;
+        double bmi = person.weight / heightInMeters / heightInMeters;
+        return bmi;
     }
 
 }
--- src/Person.java
+++ src/Person.java
@@ -1,12 +1,11 @@
 public class Person {
 
     public static final int RETIREMENT_AGE = 67;
-
     public static final int MAX_NUMBER_OF_SPOUSES = 1;
-
 
     public String name;
     public int height;
     public int weight;
     public boolean isMarried;
+    public int age;
 }
