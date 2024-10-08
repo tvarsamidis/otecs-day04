3dd1c2708721cd7bf41cfd87a4883c16afa9d171
Thomas Varsamidis <t.varsamidis@codehub.gr>
2024-10-08 12:13:19 +0300

    Added basic Person

--- src/MainApplication.java
+++ src/MainApplication.java
@@ -1,7 +1,17 @@
 public class MainApplication {
 
-    public static void main(String[] args) {
+    public static void showData(Person person) {
+        System.out.println("Name=" + person.name);
+        System.out.println("Height=" + person.height);
+    }
 
+
+    public static void main(String[] args) {
+        Person winner = new Person();
+        winner.name = "Helen";
+        winner.height = 167;
+        winner.isMarried = true;
+        showData(winner);
     }
 
 }
---
+++ src/Person.java
@@ -0,0 +1,12 @@
+public class Person {
+
+    public static final int RETIREMENT_AGE = 67;
+
+    public static final int MAX_NUMBER_OF_SPOUSES = 1;
+
+
+    public String name;
+    public int height;
+    public int weight;
+    public boolean isMarried;
+}
